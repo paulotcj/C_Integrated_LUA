3215e2f52a567c9f70202913d7ff77b563d1d808
Paulo <none@none.com>
2022-10-26 01:41:39 -0400

    .

--- 2/src/main.c
+++ 2/src/main.c
@@ -4,19 +4,30 @@
 #include "../lib/lua/src/lualib.h"
 #include "../lib/lua/src/lauxlib.h"
 
+void lua_example_dofile1(){
+    printf("something\n");
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    luaL_dofile(L, "./scripts/factorial.lua");
+    lua_close(L);
+}
 
+void lua_example_getvar1()
+{
+    printf("something\n");
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    luaL_dofile(L, "./scripts/factorial.lua");
+    lua_close(L);
+
+}
 
 int main(int argc, char *argv[])
 {
-    lua_State *L = luaL_newstate();
 
-    luaL_openlibs(L);
-
-    luaL_dofile(L, "./scripts/factorial.lua");
-
-    lua_close(L);
 
 
 
     printf("hello\n");   
+    lua_example_dofile1();
 }
